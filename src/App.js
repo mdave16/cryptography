0dbf24cc6e7c193204981b080782a78ae1a9a982
mdave16 <mdave3927@gmail.com>
2019-12-01 22:34:27 +0000

    substitution cipher

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home'
+import Substitution from './Substitution'
 import PageNotFound from './PageNotFound'
 
 class App extends Component {
@@ -14,12 +15,14 @@ class App extends Component {
 	              <Link to="/">Home</Link>
 	            </li>
 	            <li>
+	              <Link to="/substitution/">Substitution ciphers</Link>
 	            </li>
 	          </ul>
 	        </nav>
 	      </div>
 				<Switch>
 				  <Route path="/" exact component={Home} />
+				  <Route path="/substitution" component={Substitution} />
 					<Route component={PageNotFound} />
 				</Switch>
 	    </Router>
---
+++ src/Substitution.js
@@ -0,0 +1,144 @@
+import React, { Component } from "react";
+
+const removeDuplicates = (arr) => {
+	    var seen = {};
+	    var retArr = [];
+	    for (var i = 0; i < arr.length; i++) {
+	        if (!(arr[i] in seen)) {
+	            retArr.push(arr[i]);
+	            seen[arr[i]] = true;
+	        }
+	    }
+	    return retArr.join('');
+}
+const normalise = (words) => {
+	return removeDuplicates(words.toLowerCase().replace(/[^a-z]/g, ''))
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+class Substitution extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      plain: "",
+      cipher: "",
+      secret: "",
+			encrypt: 'encrypt'
+    }
+  }
+
+	encrypt = (secret, plain) => {
+		const ciphertext = normalise(secret + alphabet)
+		const fullAlphabet = alphabet + alphabet.toUpperCase()
+		const fullCiphertext = ciphertext + ciphertext.toUpperCase()
+		return plain.split('').map(x => fullAlphabet.indexOf(x) > -1 ? fullCiphertext[fullAlphabet.indexOf(x)] : x).join('')
+	}
+
+	decrypt = (secret, cipher) => {
+		const ciphertext = normalise(secret + alphabet)
+		const fullAlphabet = alphabet + alphabet.toUpperCase()
+		const fullCiphertext = ciphertext + ciphertext.toUpperCase()
+		return cipher.split('').map(x => fullCiphertext.indexOf(x) > -1 ? fullAlphabet[fullCiphertext.indexOf(x)] : x).join('')
+	}
+
+	crack = (plain, cipher) => {
+		const plainMessage = normalise(plain)
+		const cipherMessage = normalise(cipher)
+		return alphabet.split('').map(x => plainMessage.indexOf(x) > -1 ? cipherMessage[plainMessage.indexOf(x)] : '_').join('')
+	}
+
+  plainTextChange = event => {
+		if(this.state.encrypt === 'encrypt') {
+			this.setState({
+	      plain: event.target.value,
+				cipher: this.encrypt(this.state.secret, event.target.value)
+	    })
+		}
+		if(this.state.encrypt === 'crack') {
+			this.setState({
+				plain: event.target.value,
+				secret: this.crack(event.target.value, this.state.cipher)
+			})
+		}
+  }
+  cipherTextChange = event => {
+		if(this.state.encrypt === 'decrypt') {
+			this.setState({
+				plain: this.decrypt(this.state.secret, event.target.value),
+	      cipher: event.target.value
+	    })
+		}
+		if(this.state.encrypt === 'crack') {
+			this.setState({
+				secret: this.crack(this.state.plain, event.target.value),
+	      cipher: event.target.value
+	    })
+    }
+  }
+  secretTextChange = event => {
+		if(this.state.encrypt === 'encrypt') {
+			this.setState({
+        secret: event.target.value,
+				cipher: this.encrypt(event.target.value, this.state.plain)
+      })
+	  }
+		if(this.state.encrypt === 'decrypt') {
+			this.setState({
+        secret: event.target.value,
+				plain: this.decrypt(event.target.value, this.state.cipher)
+      })
+	  }
+  }
+  handleOptionChange = event => {
+    this.setState({
+      encrypt: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          <input
+            type="radio"
+            name="encrypt"
+            value="encrypt"
+            checked={this.state.encrypt === 'encrypt'}
+            onChange={this.handleOptionChange}
+          />
+          Encrypt
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="encrypt"
+            value="decrypt"
+            checked={this.state.encrypt === 'decrypt'}
+            onChange={this.handleOptionChange}
+          />
+          Decrypt
+        </label>
+				<label>
+					<input
+						type="radio"
+						name="encrypt"
+						value="crack"
+						checked={this.state.encrypt === 'crack'}
+						onChange={this.handleOptionChange}
+					/>
+					Crack
+				</label>
+        <p>Secret key: </p>
+        <input type="text" disabled={this.state.encrypt === 'crack'} onChange={this.secretTextChange} value={this.state.secret} />
+        <p>Decrypted message: </p>
+        <input type="text" disabled={this.state.encrypt === 'decrypt'} onChange={this.plainTextChange}
+				value={this.state.plain} />
+        <p>Encrypted message: </p>
+        <input type="text" disabled={this.state.encrypt === 'encrypt'} onChange={this.cipherTextChange}
+				value={this.state.cipher} />
+      </form>
+    );
+  }
+}
+export default Substitution;
