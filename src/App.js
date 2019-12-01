a414665a523226cb22510f48528ca032d699c3d3
mdave16 <mdave3927@gmail.com>
2019-12-01 19:33:37 +0000

    remove extra pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home'
-import About from './About'
-import Users from './Users'
 import PageNotFound from './PageNotFound'
 
 class App extends Component {
@@ -16,18 +14,12 @@ class App extends Component {
 	              <Link to="/">Home</Link>
 	            </li>
 	            <li>
-	              <Link to="/about/">About</Link>
-	            </li>
-	            <li>
-	              <Link to="/users/">Users</Link>
 	            </li>
 	          </ul>
 	        </nav>
 	      </div>
 				<Switch>
 				  <Route path="/" exact component={Home} />
-				  <Route path="/about/" component={About} />
-				  <Route path="/users/" component={Users} />
 					<Route component={PageNotFound} />
 				</Switch>
 	    </Router>
--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,9 @@ import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router';
 import App from './App';
 import Home from './Home'
-import About from './About'
-import Users from './Users'
 import PageNotFound from './PageNotFound'
 
-test('random shows page not found error', () => {
+test('invalid route takes me to the page not found component', () => {
   const wrapper = mount(
     <MemoryRouter initialEntries={[ '/random' ]}>
       <App/>
@@ -16,12 +14,10 @@ test('random shows page not found error', () => {
   )
 
 	expect(wrapper.find(Home)).toHaveLength(0);
-	expect(wrapper.find(About)).toHaveLength(0);
-	expect(wrapper.find(Users)).toHaveLength(0);
 	expect(wrapper.find(PageNotFound)).toHaveLength(1)
 })
 
-test('/ goes to Home', () => {
+test('valid route takes me to valid component', () => {
   const wrapper = mount(
     <MemoryRouter initialEntries={[ '/' ]}>
       <App/>
@@ -29,33 +25,5 @@ test('/ goes to Home', () => {
   )
 
 	expect(wrapper.find(Home)).toHaveLength(1);
-	expect(wrapper.find(About)).toHaveLength(0);
-	expect(wrapper.find(Users)).toHaveLength(0);
 	expect(wrapper.find(PageNotFound)).toHaveLength(0)
-})
-
-test('/about goes to About', () => {
-  const wrapper = mount(
-    <MemoryRouter initialEntries={[ '/about' ]}>
-      <App/>
-    </MemoryRouter>
-  )
-
-	expect(wrapper.find(Home)).toHaveLength(0);
-	expect(wrapper.find(About)).toHaveLength(1);
-	expect(wrapper.find(Users)).toHaveLength(0);
-	expect(wrapper.find(PageNotFound)).toHaveLength(0);
-})
-
-test('/users goes to Users', () => {
-  const wrapper = mount(
-    <MemoryRouter initialEntries={[ '/users' ]}>
-      <App/>
-    </MemoryRouter>
-  )
-
-	expect(wrapper.find(Home)).toHaveLength(0);
-	expect(wrapper.find(About)).toHaveLength(0);
-	expect(wrapper.find(Users)).toHaveLength(1);
-	expect(wrapper.find(PageNotFound)).toHaveLength(0);
 })
