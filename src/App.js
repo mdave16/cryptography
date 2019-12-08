0a30ebf80d8eea94058d1bd8ba884489ff61ff0e
mdave16 <mdave3927@gmail.com>
2019-12-08 20:12:52 +0000

    add prettier to format code and format code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Home from './Home'
-import Substitution from './Substitution'
-import PageNotFound from './PageNotFound'
+import React, { Component } from "react"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+import Home from "./Home"
+import Substitution from "./Substitution"
+import PageNotFound from "./PageNotFound"
 
 class App extends Component {
-  render() {
-    return (
+	render() {
+		return (
 			<Router basename="/cryptography">
-	      <div>
-	        <nav>
-	          <ul>
-	            <li>
-	              <Link to="/">Home</Link>
-	            </li>
-	            <li>
-	              <Link to="/substitution/">Substitution ciphers</Link>
-	            </li>
-	          </ul>
-	        </nav>
-	      </div>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/substitution/">Substitution ciphers</Link>
+							</li>
+						</ul>
+					</nav>
+				</div>
 				<Switch>
-				  <Route path="/" exact component={Home} />
-				  <Route path="/substitution" component={Substitution} />
+					<Route path="/" exact component={Home} />
+					<Route path="/substitution" component={Substitution} />
 					<Route component={PageNotFound} />
 				</Switch>
-	    </Router>
-    );
-  }
+			</Router>
+		)
+	}
 }
 
-export default App;
+export default App
