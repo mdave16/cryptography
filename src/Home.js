91b34326fcc06b54c3f10b76b878316ab3bf7fd8
mdave16 <mdave3927@gmail.com>
2020-01-12 17:16:56 +0000

    add external learning links

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,17 @@
-import React from "react"
+import React, { Fragment } from "react"
 
-const Home = () => <h2>Home</h2>
+const Home = () => (
+	<Fragment>
+		<h2>Home</h2>
+		<p>
+			Want to learn about cryptography? Try
+			<a href="https://en.wikipedia.org/wiki/Cryptography">wikipedia</a> or
+			<a href="https://www.khanacademy.org/computing/computer-science/cryptography">
+				Khan Academy
+			</a>
+			.
+		</p>
+	</Fragment>
+)
 
 export default Home
