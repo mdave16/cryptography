9615121ddb4d186ab650a46d43fa42ce4c4e4c6c
mdave16 <mdave3927@gmail.com>
2020-01-12 16:58:56 +0000

    remove state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component, Fragment } from "react"
+import React, { Fragment } from "react"
 import { Route, Switch } from "react-router-dom"
 import Home from "./Home"
 import NavigationBar from "./NavigationBar"
 import Substitution from "./Substitution"
 import PageNotFound from "./PageNotFound"
 
-class App extends Component {
-	render() {
-		return (
-			<Fragment>
-				<NavigationBar />
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/substitution" component={Substitution} />
-					<Route component={PageNotFound} />
-				</Switch>
-			</Fragment>
-		)
-	}
-}
+const App = () => (
+	<Fragment>
+		<NavigationBar />
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/substitution" component={Substitution} />
+			<Route component={PageNotFound} />
+		</Switch>
+	</Fragment>
+)
 
 export default App
